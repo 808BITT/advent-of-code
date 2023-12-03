288fcd4b101110dd8b81d5aaa6c9e3fddd076de0
808BiTT <bobbitt808@gmail.com>
2023-12-02 22:53:48 -0500

    Refactor version2 as main

--- day1/part1.go
+++ day1/part1.go
@@ -9,8 +9,37 @@ import (
 )
 
 func main() {
-	version1()
-	version2()
+	startTime := time.Now()
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	var sum int = 0
+	for _, line := range lines {
+		var left, right int = 0, 0
+		for i, c := range line {
+			if c >= '0' && c <= '9' {
+				if left == 0 {
+					left = i
+					right = i
+				} else {
+					right = i
+				}
+			}
+		}
+		l, r := int(line[left]-'0'), int(line[right]-'0')
+		sum += l*10 + r
+	}
+	fmt.Printf("Sum: %d\n", sum)
+	fmt.Printf("Execution time: %v\n", time.Since(startTime))
 }
 
 func version1() {
@@ -46,40 +75,6 @@ func version1() {
 		l, r := int(line[left]-'0'), int(line[right]-'0')
 		new := l*10 + r
 		sum += new
-	}
-	fmt.Printf("Sum: %d\n", sum)
-	fmt.Printf("Execution time: %v\n", time.Since(startTime))
-}
-
-func version2() {
-	startTime := time.Now()
-	file, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	var sum int = 0
-	for _, line := range lines {
-		var left, right int = 0, 0
-		for i, c := range line {
-			if c >= '0' && c <= '9' {
-				if left == 0 {
-					left = i
-					right = i
-				} else {
-					right = i
-				}
-			}
-		}
-		l, r := int(line[left]-'0'), int(line[right]-'0')
-		sum += l*10 + r
 	}
 	fmt.Printf("Sum: %d\n", sum)
 	fmt.Printf("Execution time: %v\n", time.Since(startTime))
