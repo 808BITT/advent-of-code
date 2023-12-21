5815d2d3433b307de967762d40132c973c9794a7
808BiTT <bobbitt808@gmail.com>
2023-12-20 21:32:27 -0500

    no bueno

--- 2023/day19/main.go
+++ 2023/day19/main.go
@@ -16,13 +16,17 @@ func main() {
 	if err {
 		return
 	}
-
 	timer := time.Now()
 	fmt.Println("Part 1:", part1(data))
 	fmt.Println("Time taken:", time.Since(timer))
-	// timer = time.Now()
-	// fmt.Println("Part 2:", part2(data))
-	// fmt.Println("Time taken:", time.Since(timer))
+
+	data, err = readInput("sample.txt")
+	if err {
+		return
+	}
+	timer = time.Now()
+	fmt.Println("Part 2:", part2(data))
+	fmt.Println("Time taken:", time.Since(timer))
 }
 
 func readInput(filename string) ([]string, bool) {
@@ -257,6 +261,412 @@ func part1(data []string) int {
 	return sum
 }
 
+func checkGroup(group MappingGroup, wf Workflow, workflows []Workflow, buffer []MappingGroup) []MappingGroup {
+	currentWorkflow := wf
+	log.Println("Checking group:", group, "in workflow:", currentWorkflow)
+
+	// loop through rules of wf
+	for _, rule := range wf.Rules {
+		log.Println("Rule:", rule)
+		if rule.Value == 0 && rule.True == "A" {
+			log.Println("Accepted group:", group)
+			buffer = append(buffer, MappingGroup{
+				Accepted: true,
+				MinX:     group.MinX,
+				MaxX:     group.MaxX,
+				MinM:     group.MinM,
+				MaxM:     group.MaxM,
+				MinA:     group.MinA,
+				MaxA:     group.MaxA,
+				MinS:     group.MinS,
+				MaxS:     group.MaxS,
+			})
+		} else if rule.Value == 0 && rule.True == "R" {
+			log.Println("Rejected group:", group)
+			continue
+		} else if rule.Value == 0 {
+			currentWorkflow = findWorkflow(rule.True, workflows)
+			log.Println("New workflow:", currentWorkflow)
+			buffer = checkGroup(group, currentWorkflow, workflows, buffer)
+		} else if !group.Accepted {
+			log.Println("Spliting:", group)
+			// split group into 4 groups based on rule
+			switch rule.Letter {
+			case "x":
+				if rule.Symbol == "<" {
+					// split group into 2 groups
+					// group 1: minx to rule.Value
+					// group 2: rule.Value+1 to maxx
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     rule.Value,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     rule.Value + 1,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				} else if rule.Symbol == ">" {
+					// split group into 2 groups
+					// group 1: minx to rule.Value-1
+					// group 2: rule.Value to maxx
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     rule.Value - 1,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     rule.Value,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				} else {
+					// split group into 2 groups
+					// group 1: minx to rule.Value
+					// group 2: rule.Value to maxx
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     rule.Value,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     rule.Value,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				}
+			case "m":
+				if rule.Symbol == "<" {
+					// split group into 2 groups
+					// group 1: minm to rule.Value
+					// group 2: rule.Value+1 to maxm
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     rule.Value,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     rule.Value + 1,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				} else if rule.Symbol == ">" {
+					// split group into 2 groups
+					// group 1: minm to rule.Value-1
+					// group 2: rule.Value to maxm
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     rule.Value - 1,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     rule.Value,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				} else {
+					// split group into 2 groups
+					// group 1: minm to rule.Value
+					// group 2: rule.Value to maxm
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     rule.Value,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     rule.Value,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				}
+			case "a":
+				if rule.Symbol == "<" {
+					// split group into 2 groups
+					// group 1: mina to rule.Value
+					// group 2: rule.Value+1 to maxa
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     rule.Value,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     rule.Value + 1,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				} else if rule.Symbol == ">" {
+					// split group into 2 groups
+					// group 1: mina to rule.Value-1
+					// group 2: rule.Value to maxa
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     rule.Value - 1,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     rule.Value,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				} else {
+					// split group into 2 groups
+					// group 1: mina to rule.Value
+					// group 2: rule.Value to maxa
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     rule.Value,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     rule.Value,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				}
+			case "s":
+				if rule.Symbol == "<" {
+					// split group into 2 groups
+					// group 1: mins to rule.Value
+					// group 2: rule.Value+1 to maxs
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     rule.Value,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     rule.Value + 1,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				} else if rule.Symbol == ">" {
+					// split group into 2 groups
+					// group 1: mins to rule.Value-1
+					// group 2: rule.Value to maxs
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     rule.Value - 1,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     rule.Value,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				} else {
+					// split group into 2 groups
+					// group 1: mins to rule.Value
+					// group 2: rule.Value to maxs
+					group1 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     group.MinS,
+						MaxS:     rule.Value,
+					}
+					group2 := MappingGroup{
+						Accepted: false,
+						MinX:     group.MinX,
+						MaxX:     group.MaxX,
+						MinM:     group.MinM,
+						MaxM:     group.MaxM,
+						MinA:     group.MinA,
+						MaxA:     group.MaxA,
+						MinS:     rule.Value,
+						MaxS:     group.MaxS,
+					}
+					buffer = checkGroup(group1, currentWorkflow, workflows, buffer)
+					buffer = checkGroup(group2, currentWorkflow, workflows, buffer)
+				}
+			}
+		}
+
+	}
+
+	return buffer
+}
+
+func part2(data []string) int {
+	workflows, _ := parseInput(data)
+	log.Println("Workflows:", workflows)
+
+	inWorkflow := findStartingWorkflow(workflows)
+	log.Println("Starting workflow:", inWorkflow)
+
+	initial := MappingGroup{
+		MinX: 1,
+		MaxX: 4000,
+		MinM: 1,
+		MaxM: 4000,
+		MinA: 1,
+		MaxA: 4000,
+		MinS: 1,
+		MaxS: 4000,
+	}
+
+	groups := checkGroup(initial, inWorkflow, workflows, []MappingGroup{})
+	log.Println("Groups:", groups)
+	return 0
+}
+
 type Workflow struct {
 	Name  string
 	Rules []Rule
@@ -274,4 +684,16 @@ type Part struct {
 	m int
 	a int
 	s int
+}
+
+type MappingGroup struct {
+	Accepted bool
+	MinX     int
+	MaxX     int
+	MinM     int
+	MaxM     int
+	MinA     int
+	MaxA     int
+	MinS     int
+	MaxS     int
 }
